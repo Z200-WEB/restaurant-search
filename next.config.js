d51cde50cd8f73c778e0e600462645596eb0e6d1
Zawe Zaw Htet <doomsday77923@gmail.com>
2026-06-03 03:36:57 +0900

    Fix: add imgfp.hotp.jp to Next.js image remote patterns to show photos

--- next.config.js
+++ next.config.js
@@ -1,17 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.hotpepper.jp',
-      },
-      {
-        protocol: 'http',
-        hostname: '**.hotpepper.jp',
-      },
-    ],
-  },
+    images: {
+          remotePatterns: [
+            {
+                      protocol: 'https',
+                      hostname: '**.hotpepper.jp',
+            },
+            {
+                      protocol: 'http',
+                      hostname: '**.hotpepper.jp',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'imgfp.hotp.jp',
+            },
+            {
+                      protocol: 'http',
+                      hostname: 'imgfp.hotp.jp',
+            },
+            {
+                      protocol: 'https',
+                      hostname: '**.hotp.jp',
+            },
+            {
+                      protocol: 'http',
+                      hostname: '**.hotp.jp',
+            },
+                ],
+    },
 };
 
 module.exports = nextConfig;
